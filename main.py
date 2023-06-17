702a0e42853797b5c232ef069836b67c81ca59a3
İsmail Tarım <68031925+ismailtrm@users.noreply.github.com>
2023-06-17 23:18:41 +0300

    Update main.py
    comment lines added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Importing necessary libraries
 import tweepy
 import logging
 from selenium import webdriver
@@ -7,11 +8,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import openai
 
+# Seting up the logging module
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
 class TwitterBot:
     def __init__(self, bearer_token, api_key, api_secret, access_token, access_token_secret, openai_key):
+        # Initialize the bot's credentials
         self.bearer_token = bearer_token
         self.api_key = api_key
         self.api_secret = api_secret
@@ -24,6 +27,7 @@ class TwitterBot:
         self.client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
 
     def setup_twitter_api(self):
+        # Set up the Twitter API authentication and API object
         self.auth = tweepy.OAuth1UserHandler(consumer_key=self.api_key,
                                              consumer_secret=self.api_secret,
                                              access_token=self.access_token,
@@ -31,26 +35,34 @@ class TwitterBot:
         self.api = tweepy.API(self.auth)
 
     def setup_selenium(self):
+        # Set up the Selenium webdriver using Chrome
         service = Service("C:\Program Files (x86)\chromedriver.exe")
         self.driver = webdriver.Chrome(service=service)
 
 
     def retrieve_trending_topics(self):
         try:
+            # Navigate to Twitter's trending topics page using Selenium
             self.driver.get("https://twitter.com/i/trends")
             wait = WebDriverWait(self.driver, 10)
+            # Find the elements containing the trending topics using CSS selector
             trend_items = wait.until(EC.presence_of_all_elements_located(
-                (By.CSS_SELECTOR, '.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0')))
-            choices = [item.text.strip() for item in trend_items[:42]]
-            trends = [choices[17], choices[20], choices[23], choices[26], choices[29]]
+                (By.CSS_SELECTOR, '.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0'))) 
+            # Extract the text of the trending topics
+            choices = [item.text.strip() for item in trend_items[:42]] # The CSS selection is not optimized. There is a bunch of things that is under that class therefore we detect the first 10 trending topics for an experiment.
+            # Choose a subset of trending topics to focus on
+            trends = [choices[17], choices[20], choices[23], choices[26], choices[29]] #Top 5 trending topics.
             return trends
         except Exception as e:
             logging.error("Failed to retrieve trending topics: %s", str(e))
             return []
+        
+        # NOTE: The free twitter api does not allow me to pull trend topics from the api due to the access leveling that's why I had to do web scraping instead of get_place_trends()
 
     def generate_tweet(self, trend):
-        prompt = f"Türkiyede şu anda Twitterda gündemde '{trend}' konusu var. Bana bununla alakalı tweet yaz."
+        prompt = f"Türkiyede şu anda Twitterda gündemde '{trend}' konusu var. Bana bununla alakalı tweet yaz." # In Turkey, '{trend}' is currently on the agenda on Twitter. Tweet me about it.
         try:
+            # Generate a tweet using the OpenAI GPT-3 model
             response = openai.Completion.create(
                 engine="text-davinci-003",
                 prompt=prompt,
@@ -68,6 +80,7 @@ class TwitterBot:
 
     def post_tweets(self, trends):
         try:
+            # Iterate over the trends and post tweets for each trend
             for i, trend in enumerate(trends):
                 tweet = self.generate_tweet(trend)
                 if tweet:
@@ -78,25 +91,35 @@ class TwitterBot:
 
     def run(self):
         logging.info("Execution has been started.")
+        # Set up the Twitter API
         self.setup_twitter_api()
         logging.info("Twitter API has beed setted up.")
+        # Set up Selenium
         self.setup_selenium()
         logging.info("Selenium has beed setted up.")
+        # Retrieve trending topics
         trends = self.retrieve_trending_topics()
         logging.info("Trends has been detected.")
+        # Post tweets
         self.post_tweets(trends)
+        # Quit Selenium webdriver
         self.driver.quit()
         logging.info("Execution has been finished.")
 
+        # NOTE: I also used selenium webdriver as a debug module
 
 if __name__ == "__main__":
+    # Twitter API credentials
     bearer_token = r"YOUR_BEARER_TOKEN"
     api_key = "YOUR_CONSUMER_KEY"
     api_secret = "YOUR_CONSUMER_SECRET"
     access_token = "YOUR_ACCESS_TOKEN"
     access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
+    # OpenAI API key
     openai_key = "YOUR_OPENAI_KEY"
-
+    
+    # Initialize the TwitterBot object
     bot = TwitterBot(bearer_token, api_key, api_secret, access_token, access_token_secret, openai_key)
+    # Run the bot
     bot.run()
 
